8ab1ea2d30645e94a8ace60c8955f30cca29e3ff
Robert Wunderer <robert@ramsalt.com>
2022-03-21 12:08:15 +0000

    Revert "Add geoip2 library installation"
    geiop2 installation has been moved to ansible
    
    This reverts commit 1ace27c0bc1e2c39d6281a2cff4a0baeffc1f385.

--- deploy/bin/deploy.sh
+++ deploy/bin/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-PHP_VERSION="8.1"
-MAXMINDDB_VERSION="${PHP_VERSION}-1.6.0-v2"
 MATOMO_VERSION="${1:-}"
 
 if [ -z "${MATOMO_VERSION}" ]; then
@@ -11,11 +9,8 @@ if [ -z "${MATOMO_VERSION}" ]; then
 fi
 
 NGINX_CONF="/etc/nginx/conf.d/default.conf"
-PHP_CONF="/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
-MATOMO_CON="/srv/www/matomo/matomo/config/config.ini.php"
-
-PHP_EXT_DIR="$(php-config --extension-dir)"
-PHP_BASE_DIR="$(dirname $(php-config --ini-path))"
+PHP_CONF="/etc/php/8.1/fpm/pool.d/www.conf"
+MATOMO_CONF="/srv/www/matomo/matomo/config/config.ini.php"
 
 RELOAD_NGINX=0
 RELOAD_PHP=0
@@ -31,17 +26,6 @@ fi
 # Update php configuration
 if ! diff -q php/www.conf "${PHP_CONF}" >/dev/null ; then
     cp php/www.conf "${PHP_CONF}"
-    RELOAD_PHP=1
-fi
-
-# Install geoip2
-if [ ! -f ${PHP_EXT_DIR}/maxminddb.so -o ! -f $PHP_BASE_DIR/fpm/conf.d/*maxminddb.ini ] ; then
-    curl -Sso "maxminddb-${MAXMINDDB_VERSION}.tar.gz" "https://github.com/ramsalt/geoip2-php/releases/download/v2/maxminddb-${MAXMINDDB_VERSION}.tar.gz"
-    tar -C "${PHP_EXT_DIR}" -xzf "maxminddb-${MAXMINDDB_VERSION}.tar.gz"
-
-    echo "extension=maxminddb.so" > $PHP_BASE_DIR/mods-available/maxminddb.ini
-    ln -s $PHP_BASE_DIR/mods-available/maxminddb.ini $PHP_BASE_DIR/fpm/conf.d/50-maxminddb.ini
-
     RELOAD_PHP=1
 fi
 
@@ -84,5 +68,5 @@ chmod 664 \
     matomo/matomo.js \
     matomo/config/config.ini.php
 
-[ "${RELOAD_PHP}" -eq "0" ] || sudo /bin/systemctl reload php${PHP_VERSION}-fpm
+[ "${RELOAD_PHP}" -eq "0" ] || sudo /bin/systemctl reload php8.1-fpm
 [ "${RELOAD_NGINX}" -eq "0" ] || sudo /bin/systemctl reload nginx
