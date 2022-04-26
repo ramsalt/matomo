2b0a0fd983c5b3ffa88e7c8804faf8d0c11872b1
Robert Wunderer <robert@ramsalt.com>
2022-04-26 12:37:48 +0000

    Use default database adapter

--- config/config.tpl.php
+++ config/config.tpl.php
@@ -6,7 +6,6 @@ username = "${MATOMO_DATABASE_NAME}"
 password = "${MATOMO_DATABASE_PASSWORD}"
 dbname = "${MATOMO_DATABASE_NAME}"
 tables_prefix = "matomo_"
-adapter = "MYSQLI"
 enable_ssl = ${MATOMO_ENABLE_DATABASE_SSL}
 ssl_ca = "${MATOMO_DATABASE_SSL_CA_FILE}"
 ssl_cert = "${MATOMO_DATABASE_SSL_CERT_FILE}"
