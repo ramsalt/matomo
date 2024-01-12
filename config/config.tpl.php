68924a6ec6744e4b09631824528db3fc2b13cb01
Robert Wunderer <robert@ramsalt.com>
2024-01-12 17:37:36 +0000

    Disable update notifications to users

--- config/config.tpl.php
+++ config/config.tpl.php
@@ -20,6 +20,11 @@ salt = "4325a52bf1bc4bad5ed57c76d13aed56"
 proxy_client_headers[] = "${MATOMO_PROXY_CLIENT_HEADER}"
 proxy_host_headers[] = "${MATOMO_PROXY_HOST_HEADER}"
 
+; do not show update notification in web frontend to normal users
+show_update_notification_to_superusers_only = 1
+; do not send email when updates are available
+enable_update_communication = 0
+
 minimum_memory_limit_when_archiving = ${MATOMO_MEMORY_LIMIT_WHEN_ARCHIVING}
 
 enable_browser_archiving_triggering = 0
