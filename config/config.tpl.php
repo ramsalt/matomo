dbf0305ba948e2ba308b4276715fef6302ad1907
Robert Wunderer <robert@ramsalt.com>
2026-04-13 16:55:08 +0200

    Instead of stderr write logs to log files in data directory
    Writing to stderr seems to mess up api results in some cases

--- config/config.tpl.php
+++ config/config.tpl.php
@@ -81,11 +81,10 @@ datatable_archiving_maximum_rows_subtable_events = 100
 datatable_archiving_maximum_rows_userid_users = 500000
 
 [log]
-log_writers[] = "errorlog"
 log_writers[] = "file"
 log_writers[] = "screen"
 ; log_level = INFO
-logger_file_path = /tmp/logs/matomo.log
+logger_file_path = tmp/logs/matomo.log
 
 [CustomReports]
 custom_reports_max_execution_time = 2700
